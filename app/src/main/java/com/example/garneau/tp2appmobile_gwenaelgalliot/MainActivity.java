ae52cbf7972a038e9d070a91c00befcccf84c480
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-09-26 15:34:07 -0400

    first commit

---
+++ app/src/main/java/com/example/garneau/tp2appmobile_gwenaelgalliot/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.garneau.tp2appmobile_gwenaelgalliot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
